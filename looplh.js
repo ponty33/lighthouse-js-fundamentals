8236aecbc3ed069b549323778ee93dacfcccba6e
Frank Chung <ponty33@gmail.com>
2018-08-21 23:04:31 +0000

    from new computer

---
+++ looplh.js
@@ -0,0 +1,17 @@
+function loopyLighthouse(range, multiples, words) {
+  for (var i = range[0]; i <= range[1]; i++) {
+    if (i % 2 === 0 && i % 5 === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % 2 === 0) {
+      console.log(words[0]);
+    } else if (i % 5 === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+
